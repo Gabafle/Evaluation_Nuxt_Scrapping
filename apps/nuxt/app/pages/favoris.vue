0d0c3a6d96189a5ec2836f9435766da6d2aa7d73
Gabafle <gabriel.peter@hotmail.fr>
2026-04-09 15:02:42 +0200

    ADD Affichage Favoris, Récupération des favoirs,Par rapports aux login

--- apps/nuxt/app/pages/favoris.vue
+++ apps/nuxt/app/pages/favoris.vue
@@ -1,17 +1,42 @@
 <script setup lang="ts">
-import type {Offer} from "~/composables/useOffers";
-import {OfferCard} from "#components";
+import type { Offer } from '~/composables/useOffers'
+import { OfferCard } from '#components'
+import { useAuth } from '~/composables/useAuth'
 
-const offers = ref<Offer[]>([])
+const { authUser } = useAuth()
+const { data: favorites } = await useAsyncData<Favorite[]>(
+  'userFavorites',
+  () => getUserFavorites(authUser.value?.id as number)
+)
 
+const { data: offers } = await useAsyncData<Offer[]>('offers', async () => {
+  const config = useRuntimeConfig()
+  const base = config.public.apiBase as string
+  const url = `${base.replace(/\/$/, '')}/offers`
+  return $fetch<Offer[]>(url)
+})
+
+const favoriteOffers = computed(() => {
+  if (!favorites.value || !offers.value) return []
+
+  const favoriteIds = new Set(favorites.value.map(f => f.offerId))
+  return offers.value.filter(offer => favoriteIds.has(offer.id))
+})
+
+if (!authUser.value) {
+  navigateTo('/login')
+}
 </script>
 
 <template>
-<h1>FAVORIS</h1>
-
-
+  <div class="p-4">
+    <h1>Mes favoris</h1>
+    <div class="grid">
+      <OfferCard
+        v-for="offer in favoriteOffers"
+        :key="offer.id"
+        :offer="offer"
+      />
+    </div>
+  </div>
 </template>
-
-<style scoped>
-
-</style>
