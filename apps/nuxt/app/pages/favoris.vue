abc034a54b60bd919da5f45d3c04ae5b136d88b6
Gabafle <gabriel.peter@hotmail.fr>
2026-04-09 14:19:58 +0200

    ADD Pages Favoris Empty And Button

---
+++ apps/nuxt/app/pages/favoris.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type {Offer} from "~/composables/useOffers";
+import {OfferCard} from "#components";
+
+const offers = ref<Offer[]>([])
+
+</script>
+
+<template>
+<h1>FAVORIS</h1>
+
+
+</template>
+
+<style scoped>
+
+</style>
